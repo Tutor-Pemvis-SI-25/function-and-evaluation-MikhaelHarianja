c1483b60edc9dc0889cea116ba3706c8ab6b5b15
mikhaelharianja53-star <mikhaelharianja07@gmail.com>
2025-11-21 09:40:09 +0700

    Update TLAST.java

--- TLAST.java
+++ TLAST.java
@@ -1,6 +1,5 @@
 // Miransya Gresia Siagian - 12S25031
 // Mikhael Jordania Harianja - 12S25053
-
 import java.util.*;
 import java.lang.Math;
 
@@ -46,8 +45,8 @@ public class TLAST {
         dosen[i] = input.nextLine();
         deadline[i] = input.nextLine();
         nimnama[i] = input.nextLine();
-        tingkatkesulitan[i] = input.nextDouble();
-        harihinggadeadline[i] = input.nextInt();
+        tingkatkesulitan[i] = Double.parseDouble(input.nextLine());
+        harihinggadeadline[i] = Integer.parseInt(input.nextLine());
         status[i] = input.nextLine();
         prioritas[i] = tingkatkesulitan[i] * 1.0 / harihinggadeadline[i];
         if (prioritas[i] > 3) {
